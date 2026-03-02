4d8a241efdb9c80b16401f874068ccf34901ec60
Abel <106527774+AbelLiberato@users.noreply.github.com>
2026-03-01 22:03:13 -0300

    Add files via upload

---
+++ ConsumerAdviceApi/AdviceResponse.cs
@@ -0,0 +1,13 @@
+namespace ConsumerAdviceApi
+{
+    public class AdviceResponse
+    {
+        public Slip slip { get; set; }
+    }
+
+    public class Slip
+    {
+        public int id { get; set; }
+        public string advice { get; set; }
+    }
+}
